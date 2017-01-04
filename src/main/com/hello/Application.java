aa60e4e3d26d46707a5044bfbd8ff494c5e51798
Administrator <2416762675@qq.com>
2017-01-05 01:18:10 +0800

    First

---
+++ src/main/com/hello/Application.java
@@ -0,0 +1,23 @@
+package com.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Application {
+    @RequestMapping("/hi")
+    public String showUserInfo(){
+
+        return  "index";
+    }
+
+
+    @RequestMapping("/apps.do")
+    public String showAction(){
+
+        return  "Hello";
+    }
+}
+
+
+
